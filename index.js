e1a48ff505b0d9bee7f3c8703ad929b33bb7fbe7
Mister-Kay <28190493+Mister-Kay@users.noreply.github.com>
2017-10-07 11:54:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = function mountFix(dispatch) {
+	// variables
+	let mountTime = 0
+	
+	dispatch.hook('S_MOUNT_VEHICLE', 'raw', code => {
+		mountTime = Date.now()
+	})
+	
+	dispatch.hook('C_UNMOUNT_VEHICLE', 'raw', code => {
+		if ((Date.now() - mountTime) < 300) {
+			return false
+		}
+	})
+}
