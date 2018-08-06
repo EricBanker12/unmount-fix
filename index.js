baf0ec9e09c057d389a6b27de58b061566a42c5b
Mister-Kay <private-information>
2018-08-06 12:40:57 -0500

    compatability fix for mount-prediction

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ module.exports = function mountFix(dispatch) {
 	// variables
 	let mountTime = 0
 	
-	dispatch.hook('S_MOUNT_VEHICLE', 'raw', code => {
+	dispatch.hook('S_MOUNT_VEHICLE', 'raw', {order: -100, filters: {fake: null}}, code => {
 		mountTime = Date.now()
 	})
 	
-	dispatch.hook('C_UNMOUNT_VEHICLE', 'raw', code => {
+	dispatch.hook('C_UNMOUNT_VEHICLE', 'raw', {order: -100}, code => {
 		if ((Date.now() - mountTime) < 300) {
 			return false
 		}
 	})
 
-	dispatch.hook('C_START_SKILL', 'raw', code => {
+	dispatch.hook('C_START_SKILL', 'raw', {order: -100}, code => {
 		if ((Date.now() - mountTime) < 300) {
 			return false
 		}
